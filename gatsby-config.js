1489a6c60da32d4795f4201b3ea9885d4948fdb0
Richard Zilahi <zilahi@gmail.com>
2018-12-27 20:45:11 +0200

    GA tracking code added

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-128990219-1`,
       },
     },
     `gatsby-plugin-feed`,
